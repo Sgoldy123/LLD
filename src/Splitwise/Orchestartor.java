342f9775994383afe68e1eaf46114a6378a0e1a3
soumya.sasmal <soumya.sasmal@ril.com>
2024-08-19 20:27:17 +0530

    ese hi

---
+++ src/Splitwise/Orchestartor.java
@@ -0,0 +1,20 @@
+package Splitwise;
+
+import java.util.Map;
+
+public class Orchestartor {
+
+    public static void main(String args[]){
+        User sahil = new User("Sahil");
+        User soumya = new User("Soumya");
+        User abhay = new User("Abhay");
+
+        List<User> 
+//        Balance test = sahil.getBalance();
+//        test.getGetback().put(sahil,100);
+//        test.getGetback().put(sahil,500);
+//        for(Map.Entry<User,Integer> it: test.getGetback().entrySet()){
+//            System.out.println(it.getKey().getId() +" " +it.getValue());
+//        }
+    }
+}
