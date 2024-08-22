95c408c80faf9b5bddb08d762e9fb01348e7eb81
soumya.sasmal <soumya.sasmal@ril.com>
2024-08-22 21:20:32 +0530

    synchronized

--- src/Key_Value_Store/Orchestrator.java
+++ src/Key_Value_Store/Orchestrator.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 
 public class Orchestrator {
     private static Data data = Data.getData();
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while(sc.hasNext()) {
-            String line = sc.nextLine();
-            processCommand(line);
 
-            //data.print();
-        }
-    }
 
-    public static synchronized void processCommand(String line){
+    public synchronized static void processCommand(String line){
+
         String[] inputArray = line.split(" ");
         if(inputArray[0].equals("put"))data.putKey(inputArray);
         else if(inputArray[0].equals("get"))data.getKey(inputArray);
         else if(inputArray[0].equals("delete"))data.deleteKey(inputArray);
         else if(inputArray[0].equals("search"))data.searchKeyVal(inputArray);
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+        data.print();
+        System.out.println("_______NEW OUTPUT___________");
+        //Thread.sleep(2);
     }
 }
