120e71ca3a10ae6fed6512da1b4c5d49e7613a02
soumya prakash <ssoumyaprakash05@gmail.com>
2025-01-29 00:35:29 +0530

    updates

--- src/main/java/snakeLadder/Dice.java
+++ src/main/java/snakeLadder/Dice.java
@@ -3,9 +3,9 @@ package snakeLadder;
 public class Dice
 {
 
-    public static int diceRole(){
-
-       return ((int)(Math.random()*5))+1;
+    public static int diceRole() {
+        return (int)(Math.random() * 6) + 1;
     }
+
 }
-//0.9999*11
+//0.9999*6--> (0-5.9999) ---> (int) ---> (0-5) ---> +1 ----> (1-6)
